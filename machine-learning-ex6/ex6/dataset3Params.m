e7d0f6666f22764d6aa57079b94f2a6cfc6c6096
Eugene Lee <eugene097@csu.fullerton.edu>
2020-08-16 14:12:48 -0700

    Adding ex6

---
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -0,0 +1,56 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+testC = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+testSigma = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+min_prediction_error = inf;
+
+% This runs through 64 iterations so it's going to take a while
+for i = 1:length(testC)
+    for j = 1:length(testSigma)
+        C_test = testC(i);
+        sigma_test = testSigma(j);
+
+    model = svmTrain(X, y, C_test, @(x1,x2) gaussianKernel(x1,x2,sigma_test));
+    predictions = svmPredict(model,Xval);
+    prediction_error = mean(double(predictions ~= yval));
+    
+    if(prediction_error < min_prediction_error)
+        optimal_C = C_test;
+        optimal_sigma = sigma_test;
+        min_prediction_error = prediction_error;
+    end
+  end
+end
+
+C = optimal_C;
+sigma = optimal_sigma;
+
+
+
+
+% =========================================================================
+
+end
